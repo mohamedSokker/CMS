3a8426c5d87137210700175ff5928d894472abd0
mohamedSokker <mhmd.sokkar92@gmail.com>
2025-06-28 12:10:56 +0300

    Try Login Cookies V5

--- backend/v3/middlewares/credentials.js
+++ backend/v3/middlewares/credentials.js
@@ -2,8 +2,10 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
+  console.log("Origin:", origin);
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", origin);
   }
   next();
 };
